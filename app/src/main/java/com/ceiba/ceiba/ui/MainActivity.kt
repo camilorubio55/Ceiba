6dae4b4c611cd4bba6a9ee8cdd2fc4d8453129e7
camilorubio <camilorubiocontreras@gmail.com>
2020-10-17 20:55:57 -0500

    first commit

---
+++ app/src/main/java/com/ceiba/ceiba/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ceiba.ceiba.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ceiba.ceiba.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
